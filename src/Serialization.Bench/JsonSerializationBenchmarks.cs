447a063a7ff05919969b9a97756e7ac8df0d41e1
toms-vanders <toms.vanders@gmail.com>
2025-10-23 15:35:00 +0200

    Uncommented benchark methods

--- src/Serialization.Bench/JsonSerializationBenchmarks.cs
+++ src/Serialization.Bench/JsonSerializationBenchmarks.cs
@@ -38,50 +38,50 @@ public class JsonSerializationBenchmarks
     {
         return JsonConvert.SerializeObject(_smallPayload);
     }
-    //
-    // [Benchmark]
-    // public string Jil_Serialize_Small()
-    // {
-    //     return JSON.Serialize(_smallPayload);
-    // }
-    //
-    // [Benchmark]
-    // public string Utf8Json_Serialize_Small()
-    // {
-    //     return Utf8Json.JsonSerializer.ToJsonString(_smallPayload);
-    // }
-    //
-    // [Benchmark]
-    // public string SpanJson_Serialize_Small()
-    // {
-    //     return SpanJson.JsonSerializer.Generic.Utf16.Serialize(_smallPayload);
-    // }
-    // [Benchmark]
-    // public string SystemTextJson_Serialize_Medium()
-    // {
-    //     return System.Text.Json.JsonSerializer.Serialize(_mediumPayload);
-    // }
-    //
-    // [Benchmark]
-    // public string Newtonsoft_Serialize_Medium()
-    // {
-    //     return JsonConvert.SerializeObject(_mediumPayload);
-    // }
-    //
-    // [Benchmark]
-    // public string SystemTextJson_Serialize_Large()
-    // {
-    //     return System.Text.Json.JsonSerializer.Serialize(_largePayload);
-    // }
-    //
-    // [Benchmark]
-    // public string Newtonsoft_Serialize_Large()
-    // {
-    //     return JsonConvert.SerializeObject(_largePayload);
-    // }
-    // [Benchmark]
-    // public SmallPayload SystemTextJson_Deserialize_Small()
-    // {
-    //     return System.Text.Json.JsonSerializer.Deserialize<SmallPayload>(_smallJson);
-    // }
+    
+    [Benchmark]
+    public string Jil_Serialize_Small()
+    {
+        return JSON.Serialize(_smallPayload);
+    }
+    
+    [Benchmark]
+    public string Utf8Json_Serialize_Small()
+    {
+        return Utf8Json.JsonSerializer.ToJsonString(_smallPayload);
+    }
+    
+    [Benchmark]
+    public string SpanJson_Serialize_Small()
+    {
+        return SpanJson.JsonSerializer.Generic.Utf16.Serialize(_smallPayload);
+    }
+    [Benchmark]
+    public string SystemTextJson_Serialize_Medium()
+    {
+        return System.Text.Json.JsonSerializer.Serialize(_mediumPayload);
+    }
+    
+    [Benchmark]
+    public string Newtonsoft_Serialize_Medium()
+    {
+        return JsonConvert.SerializeObject(_mediumPayload);
+    }
+    
+    [Benchmark]
+    public string SystemTextJson_Serialize_Large()
+    {
+        return System.Text.Json.JsonSerializer.Serialize(_largePayload);
+    }
+    
+    [Benchmark]
+    public string Newtonsoft_Serialize_Large()
+    {
+        return JsonConvert.SerializeObject(_largePayload);
+    }
+    [Benchmark]
+    public SmallPayload SystemTextJson_Deserialize_Small()
+    {
+        return System.Text.Json.JsonSerializer.Deserialize<SmallPayload>(_smallJson);
+    }
 }
